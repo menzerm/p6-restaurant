79810293dfab0a3a0fcd5dd114682bc2fea76ae8
menzerm <myra@menzer.biz>
2014-06-01 20:49:48 -0700

    version 0

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// My Scripts
+$(document).foundation();
+
+$('a').click(function() {
+	$ ('article').readmore():
+});
+
+$('article').readmore({
+	speed: 1000,
+	maxHeight:0,
+	startOpen:true,
+});
