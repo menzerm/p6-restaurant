5d09118c4daac7ce0038ef93cfc8de02624f62a9
Menzer <mmenzer@it-mmc-m10.eastern.ewu.edu>
2014-06-02 12:10:24 -0700

    MixItUp

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,7 @@
 // My Scripts
 $(document).foundation();
 
-$('a').click(function() {
-	$ ('article').readmore():
-});
-
-$('article').readmore({
-	speed: 1000,
-	maxHeight:0,
-	startOpen:true,
+$(document).ready(function(){
+	$(function(){
+  		$('#Container').mixItUp();
 });
